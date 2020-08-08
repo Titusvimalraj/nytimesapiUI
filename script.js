350c24e97c4d2ace4ec2ccb2c5ac4c3f06a76b41
Titus Vimal Raj <titusvimalraj@gmail.com>
2020-08-09 03:35:57 +0530

    fixed issues

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let apiKey = `pqiWEDtWRSel2sbZpjoGfGA95tlGZ2aq`
 
 let htmlParentElements = () => {
     let parentsElementsInnerHtml = `<div class="container-fluid">
+    <h1>New York Times Top Stories API</h1>
     <div class="row">
         <div class="col-12 mt-5">
             <div class="accordion" id="nytimesAccordion">
@@ -60,7 +61,7 @@ let loadInitialParents = () => {
             </h2>
         </div>
     
-        <div id="${element}Collapse" class="collapse show" aria-labelledby="${element}" data-parent="#nytimesAccordion">
+        <div id="${element}Collapse" class="collapse row show" aria-labelledby="${element}" data-parent="#nytimesAccordion">
         
         </div>
         </div>
@@ -85,44 +86,52 @@ let getNewsData = async(sectionName) => {
 
         let data = await response.json();
 
-        console.log(data);
+        //console.log(data);
 
 
 
         let accordionCollapseContent = '';
 
         if (data.results.length > 0) {
+            let newCardInfo = data.results;
+            //console.log(newCardInfo);
 
-            data.results.forEach(element => {
+            for (let i = 0; i < newCardInfo.length; i++) {
+                let imgSrc = '';
+                if (newCardInfo[i].hasOwnProperty('multimedia') && newCardInfo[i]['multimedia'] != null) {
+                    //console.log(i);
+                    //console.log(newCardInfo[i].multimedia);
+                    imgSrc = newCardInfo[i].multimedia[1].url;
+                }
 
-                let currentCollapseData = `    <div class="col-8 offset-2 offset-md-1 col-md-9">
-                <div class="card mb-3">
-                ${sectionName}
-                    <div class="row no-gutters">
-                        
-                        <div class="col-md-4 order-md-1">
-                            <img src="${element.multimedia[1].url}" class="card-img" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">${element.title}</h5>
-                                <p class="card-text"><small class="text-muted">${new Date(element.published_date).toDateString()}</small></p>
-                                <p class="card-text">${element.abstract}</p>
-                                
-                                <a href="${element.short_url}" class="card-link">Continue Reading</a>
-                            </div>
-
+                let currentCollapseData = `    <div class="col-8 offset-2 offset-lg-0 col-lg-6 mt-4">
+                    <div class="card mb-3">
+                    ${sectionName}
+                        <div class="row no-gutters">
                             
-                        </div>                        
+                            <div class="col-md-4 order-md-1">
+                                <img src="${imgSrc}" class="card-img" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">${newCardInfo[i].title}</h5>
+                                    <p class="card-text"><small class="text-muted">${new Date(newCardInfo[i].published_date).toDateString()}</small></p>
+                                    <p class="card-text">${newCardInfo[i].abstract}</p>
+                                    
+                                    <a href="${newCardInfo[i].short_url}" class="card-link">Continue Reading</a>
+                                </div>
+    
+                                
+                            </div>                        
+                        </div>
                     </div>
-                </div>
-            </div>`;
+                </div>`;
 
                 accordionCollapseContent += currentCollapseData;
 
-            })
+            }
 
-            document.getElementById(`${sectionName}`).innerHTML = accordionCollapseContent;
+            document.getElementById(`${sectionName}Collapse`).innerHTML = accordionCollapseContent;
 
         }
 
@@ -146,3 +155,87 @@ let getNewsData = async(sectionName) => {
 
 
 }
+
+
+
+let getFirstData = async(sectionName = 'arts') => {
+    try {
+
+        let url = `https://api.nytimes.com/svc/topstories/v2/${sectionName}.json?api-key=${apiKey}`;
+
+        let response = await fetch(url);
+
+        let data = await response.json();
+
+        //console.log(data);
+
+
+
+        let accordionCollapseContent = '';
+
+        if (data.results.length > 0) {
+            let newCardInfo = data.results;
+            //console.log(newCardInfo);
+
+            for (let i = 0; i < newCardInfo.length; i++) {
+                let imgSrc = '';
+                if (newCardInfo[i].hasOwnProperty('multimedia') && newCardInfo[i]['multimedia'] != null) {
+                    //console.log(i);
+                    //console.log(newCardInfo[i].multimedia);
+                    imgSrc = newCardInfo[i].multimedia[1].url;
+                }
+
+                let currentCollapseData = `    <div class="col-8 offset-2 offset-lg-0 col-lg-6 mt-4">
+                    <div class="card mb-3">
+                    ${sectionName}
+                        <div class="row no-gutters">
+                            
+                            <div class="col-md-4 order-md-1">
+                                <img src="${imgSrc}" class="card-img" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">${newCardInfo[i].title}</h5>
+                                    <p class="card-text"><small class="text-muted">${new Date(newCardInfo[i].published_date).toDateString()}</small></p>
+                                    <p class="card-text">${newCardInfo[i].abstract}</p>
+                                    
+                                    <a href="${newCardInfo[i].short_url}" class="card-link">Continue Reading</a>
+                                </div>
+    
+                                
+                            </div>                        
+                        </div>
+                    </div>
+                </div>`;
+
+                accordionCollapseContent += currentCollapseData;
+
+            }
+
+            document.getElementById(`${sectionName}Collapse`).innerHTML = accordionCollapseContent;
+
+        }
+
+
+
+
+
+
+
+    } catch (error) {
+        let errorElement = document.createElement('div');
+        errorElement.className = `alert alert-warning alert-dismissible fade show`;
+        errorElement.Role = `alert`;
+        errorElement.tabIndex = 10;
+        errorElement.innerHTML = `<strong>Error!</strong> ${error}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>`
+        document.getElementById(`${sectionName}Collapse`).appendChild(errorElement);
+    }
+
+
+}
+
+
+getFirstData();
